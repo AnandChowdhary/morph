208dc9c25eb12b4b5522ad565e010d5ae00fb24c
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-26 12:46:09 -0800

    :recycle: Set up starter

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button>Login with GitHub &rarr;</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+button {
+  font: inherit;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 1.25rem;
+  border-radius: 0.2rem;
+  font-size: 150%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Morph</h1>
+    <p class="lead">Community-powered localization for open-source projects</p>
+    <Login msg="Welcome to Your Vue.js App" />
+    <p class="setup"><router-link to="/">Set up your project</router-link></p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Login from "@/components/Login.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Login
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  text-align: center;
+  margin: 5rem auto;
+  max-width: 700px;
+}
+p.lead {
+  font-size: 150%;
+  margin-bottom: 2rem;
+}
+p.setup {
+  margin-top: 2rem;
+}
+</style>
